6a7aa5156636e223d0430624d256ab9253ca50fb
James Hughes <jameshughes2009@hotmail.com>
2024-03-24 10:17:13 -0400

    starting to add JS and sql coding to connect to mysql

---
+++ server.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql2")
+const cfonts = require("cfonts")
+
+// now adding the mysql connection for the account 
+const connection =mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("you are now connected!!");
+    start(); //used to start the app
+})
