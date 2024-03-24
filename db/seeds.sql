3c3684d35e4d7253f8ef53d9b748504888310190
James Hughes <jameshughes2009@hotmail.com>
2024-03-24 14:56:53 -0400

    more project edits

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,15 @@ VALUES
 ('QA'),
 ('QC');
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('James', 'Hughes', 1, 1),
+('Jimmy', 'G', 2, 2),
+('Iron', 'Man', 3, 3),
+('James', 'Blunt', 4, 4),
+('Thomas', 'Hobbes', 5, 5),
+('Charles', 'Darwin', 6, 6),
+('JK', 'Rowling', 7, 7),
+('Elon', 'Chad', 8, 8),
+('Justin', 'Bieber', 9, 9),
+('Tom', 'Cruise', 10, 10);
