75d44caee50c29bfa9d2045abd025031416388ed
James Hughes <jameshughes2009@hotmail.com>
2024-03-26 19:04:54 -0400

    adding another view function

--- server.js
+++ server.js
@@ -516,3 +516,16 @@ function deleteEmployee() {
             });
     });
 }
+
+// Used to view Employees by Department
+function viewEmployeesByDepartment() {
+    const query =
+        "SELECT departments.department_name, employee.first_name, employee.last_name FROM employee INNER JOIN roles ON employee.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id ORDER BY departments.department_name ASC";
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\nEmployees by department:");
+        console.table(res);
+        start();
+    });
+}
